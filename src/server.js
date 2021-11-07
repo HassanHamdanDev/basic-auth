37a3b73143ee4a1bf59fec68045763659da8b74a
HassanHamdanDev <hassanhamdandev@gmail.com>
2021-11-07 19:43:47 +0200

    add

---
+++ src/server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const express = require('express');
+require('dotenv').config();
+const server = express();
+const PORT = process.env.PORT || 3004;
+
+const notFoundHandler = require('./error-handlers/404');
+const errorHandler = require('./error-handlers/500');
+
+
+
+
+server.use(express.json());
+
+server.use('*', notFoundHandler);
+server.use(errorHandler);
+
+
+
+
+function start() {
+    server.listen(PORT, () => {
+        console.log(`Server Listening on ${PORT}`);
+    });
+}
+
+module.exports = {
+    server: server,
+    start: start,
+}
